acc071f72344810104d01ed1dac1e1773ffaccfe
aksoyerol <erolaksoy98@gmail.com>
2021-04-26 21:04:53 +0300

    Add Username Validator

--- app/src/main/java/school/cactus/succulentshop/ui/SignUpActivity.kt
+++ app/src/main/java/school/cactus/succulentshop/ui/SignUpActivity.kt
@@ -1,17 +1,25 @@
 package school.cactus.succulentshop.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import school.cactus.succulentshop.databinding.ActivitySignUpBinding
 import school.cactus.succulentshop.ui.validators.EmailValidator
+import school.cactus.succulentshop.ui.validators.UserNameValidator
 import school.cactus.succulentshop.ui.validators.validate
 
 private lateinit var binding: ActivitySignUpBinding
+private val emailValidator = EmailValidator()
+private val userNameValidator = UserNameValidator()
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.signUpButton.setOnClickListener {
+            binding.emailInputLayout.validate(emailValidator)
+            binding.usernameInputLayout.validate(userNameValidator)
+        }
     }
 }
