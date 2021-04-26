8685ee0ecaad0f0b220f4238ef1e76df36b3a2ee
aksoyerol <erolaksoy98@gmail.com>
2021-04-26 21:19:55 +0300

    Add advanced password validator

--- app/src/main/java/school/cactus/succulentshop/ui/SignUpActivity.kt
+++ app/src/main/java/school/cactus/succulentshop/ui/SignUpActivity.kt
@@ -3,6 +3,7 @@ package school.cactus.succulentshop.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import school.cactus.succulentshop.databinding.ActivitySignUpBinding
+import school.cactus.succulentshop.ui.validators.AdvancedPasswordValidator
 import school.cactus.succulentshop.ui.validators.EmailValidator
 import school.cactus.succulentshop.ui.validators.UserNameValidator
 import school.cactus.succulentshop.ui.validators.validate
@@ -10,7 +11,7 @@ import school.cactus.succulentshop.ui.validators.validate
 private lateinit var binding: ActivitySignUpBinding
 private val emailValidator = EmailValidator()
 private val userNameValidator = UserNameValidator()
-
+private val advancedPasswordValidator = AdvancedPasswordValidator()
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +21,7 @@ class SignUpActivity : AppCompatActivity() {
         binding.signUpButton.setOnClickListener {
             binding.emailInputLayout.validate(emailValidator)
             binding.usernameInputLayout.validate(userNameValidator)
+            binding.passwordInputLayout.validate(advancedPasswordValidator)
         }
     }
 }
---
+++ app/src/main/java/school/cactus/succulentshop/ui/validators/AdvancedPasswordValidator.kt
@@ -0,0 +1,18 @@
+package school.cactus.succulentshop.ui.validators
+
+import school.cactus.succulentshop.R
+
+class AdvancedPasswordValidator : Validator {
+    override fun validate(field: String): Int? {
+        return when {
+            field.isEmpty() -> R.string.password_is_required
+            field.length <= 7 -> R.string.password_is_too_short
+            field.length >= 40 -> R.string.password_is_too_long
+            !field.any { it.isDigit() } -> R.string.password_must_contains_rules
+            !field.any { it.isLowerCase() } -> R.string.password_must_contains_rules
+            !field.any { it.isUpperCase() } -> R.string.password_must_contains_rules
+            !field.any { !it.isLetterOrDigit() } -> R.string.password_must_contains_rules
+            else -> null
+        }
+    }
+}
